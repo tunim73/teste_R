867c61852a6001d72ef052f9a4b1b441abf928bd
tunim73 <antonio.netto973@gmail.com>
2023-08-29 22:19:55 -0300

    predição com redes neurais

--- Teste Pratc de pesquisa.R
+++ Teste Pratc de pesquisa.R
@@ -1,13 +1,16 @@
+#Libs
+library(nnet)
+library(daltoolbox, lib.loc = "/usr/local/lib/R/site-library")
 #Importação do Dados
 dados = read.table("~/Prova de R/wine.data", sep = ",", header = TRUE,
-                    col.names = c("class","Alcohol","Malicacid","Ash","Alcalinity_of_ash","Magnesium",
-                                  "Total_phenols","Flavanoids","Nonflavanoid_phenols","Proanthocyanins",
-                                  "Color_intensity","Hue","OD280_OD315_of_diluted_wines","Proline"))
+                   col.names = c("class","Alcohol","Malicacid","Ash","Alcalinity_of_ash","Magnesium",
+                                 "Total_phenols","Flavanoids","Nonflavanoid_phenols","Proanthocyanins",
+                                 "Color_intensity","Hue","OD280_OD315_of_diluted_wines","Proline"))
 dados
 
 nome_col = c("Alcohol","Malicacid","Ash","Alcalinity_of_ash","Magnesium",
-            "Total_phenols","Flavanoids","Nonflavanoid_phenols","Proanthocyanins",
-            "Color_intensity","Hue","OD280_OD315_of_diluted_wines","Proline")
+             "Total_phenols","Flavanoids","Nonflavanoid_phenols","Proanthocyanins",
+             "Color_intensity","Hue","OD280_OD315_of_diluted_wines","Proline")
 
 #Fazendo a media e o desvio padrão de todos vinhos
 
@@ -67,8 +70,8 @@ for (nome in nome_col) {
     if(nome!=nome2){
       grf = plot_scatter(dados |> dplyr::select(x = nome,
                                                 value = nome2, variable = class),
-                          label_x = nome, label_y = nome2,
-                          colors=c("red", "green", "blue"))
+                         label_x = nome, label_y = nome2,
+                         colors=c("red", "green", "blue"))
       plot(grf)
     }
   }
@@ -77,11 +80,11 @@ for (nome in nome_col) {
 
 #Discretização dos atributos numericos
 for (nome in nome_col) {
-  nome_coluna = paste(nome,"_discretizada", sep = "")
+  nome_coluna = paste(nome,"_disc", sep = "")
   limites = quantile(dados[[nome]], probs = c(0, 1/3, 2/3, 1))
   dados[nome_coluna] = as.factor(cut(dados[[nome]], breaks = limites,
-                                          labels = c("Baixo", "Médio", "Alto"),
-                                          include.lowest = TRUE))
+                                     labels = c("Baixo", "Médio", "Alto"),
+                                     include.lowest = TRUE))
 }
 
 #Mapeamento categorico do tipo de vinho
@@ -89,22 +92,28 @@ cm = categ_mapping("class")
 dados_cm = transform(cm, dados)
 ddados = dados[,"class", drop=FALSE]
 dados_cm = transform(cm, ddados)
-
-#Modelo de Agrupamento K-Means
-
-# setup clustering
-model = cluster_kmeans(k=13)
-
-# build model
-model = fit(model, dados[,2:14])
-clu = cluster(model, dados[,2:14])
-table(clu)
-
-# evaluate model using external metric
-eval = evaluate(model, clu, dados$class)
-eval
+dados = cbind(dados_cm, dados)
 
 
 
+#Modelo de Predição
 
+set.seed(123)  # Definir uma semente para reprodução dos resultados
+sample_index = sample(nrow(dados), 0.7 * nrow(dados))  # 70% para treinamento
+train_data = dados[sample_index, ]
+test_data = dados[-sample_index, ]
 
+# Treinamento do modelo de redes neurais
+model = nnet(class ~ Alcohol + Total_phenols + Nonflavanoid_phenols, data = train_data, size = 5)
+
+# Fazer previsões no conjunto de teste
+predicted_probs = round(predict(model, newdata = test_data, type = "raw"),8)
+predicted_classes = colnames(predicted_probs)[apply(predicted_probs, 1, which.max)]
+
+# Calcular a acurácia
+accuracy = round(mean(predicted_classes == test_data$class),4)
+cat("Acurácia:", accuracy, "\n")
+
+#Dados reais X Dados da Previsão
+results <- data.frame(Real = test_data$class, Previsão = predicted_classes)
+print(results)
